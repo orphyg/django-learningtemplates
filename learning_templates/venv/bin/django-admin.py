4456045eb5830fdaaeb4741f52ae549c2e5f21ef
Gamal Orphy <Home@GamalsMacBook.local>
2019-06-09 11:38:11 +0200

    first learning templates

---
+++ learning_templates/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Home/Desktop/Web_Dev/Django-Python-Full-Stack-Web-Devloper-master/Django_Level_Four/learning_templates/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
